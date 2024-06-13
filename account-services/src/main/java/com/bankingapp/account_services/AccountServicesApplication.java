a141f3b1afc4d57748125090718d4b8b7ae3a786
aathil12346 <129156477+aathil12346@users.noreply.github.com>
2024-06-13 16:44:33 +0100

    initial commit

---
+++ account-services/src/main/java/com/bankingapp/account_services/AccountServicesApplication.java
@@ -0,0 +1,13 @@
+package com.bankingapp.account_services;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountServicesApplication.class, args);
+	}
+
+}
---
+++ transaction-services/src/main/java/com/bankingapp/transaction_services/TransactionServicesApplication.java
@@ -0,0 +1,13 @@
+package com.bankingapp.transaction_services;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransactionServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransactionServicesApplication.class, args);
+	}
+
+}
