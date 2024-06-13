bb5e46a37c4fcb95c2877617a2448f1218a68b4c
aathil12346 <129156477+aathil12346@users.noreply.github.com>
2024-06-13 17:25:36 +0100

    created a s3fileuploadservice class which is where the logic to upload a file to s3 goes

---
+++ account-services/src/main/java/com/bankingapp/account_services/service/impl/S3FileUploadServiceImpl.java
@@ -0,0 +1,8 @@
+package com.bankingapp.account_services.service.impl;
+
+import com.bankingapp.account_services.service.S3FileUploadService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class S3FileUploadServiceImpl implements S3FileUploadService {
+}
