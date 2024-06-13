83b655e6b8492a9885d808f5132c756fb2b6d24a
aathil12346 <129156477+aathil12346@users.noreply.github.com>
2024-06-13 17:38:41 +0100

    created the upload file business logic in the S3 service class

--- account-services/src/main/java/com/bankingapp/account_services/service/S3FileUploadService.java
+++ account-services/src/main/java/com/bankingapp/account_services/service/S3FileUploadService.java
@@ -1,4 +1,10 @@
 package com.bankingapp.account_services.service;
 
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
 public interface S3FileUploadService {
+
+    String uploadFile(MultipartFile file) throws IOException;
 }
--- account-services/src/main/java/com/bankingapp/account_services/service/impl/S3FileUploadServiceImpl.java
+++ account-services/src/main/java/com/bankingapp/account_services/service/impl/S3FileUploadServiceImpl.java
@@ -1,10 +1,44 @@
 package com.bankingapp.account_services.service.impl;
 
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.PutObjectRequest;
 import com.bankingapp.account_services.service.S3FileUploadService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Objects;
 
 @Service
 public class S3FileUploadServiceImpl implements S3FileUploadService {
 
 
+    @Autowired
+    private AmazonS3 s3Client;
+
+    @Value("${app.s3.bucket}")
+    private String bucketName;
+
+
+    @Override
+    public String uploadFile(MultipartFile file) throws IOException {
+        File fileObj = convertMultiPartFileToFile(file);
+        String filename = System.currentTimeMillis() + "_" + file.getOriginalFilename();
+        s3Client.putObject(new PutObjectRequest(bucketName,filename,fileObj));
+        fileObj.delete();
+        return "file uploaded to s3 successfully";
+    }
+
+
+    private File convertMultiPartFileToFile(MultipartFile file) throws IOException {
+        File convertedFile = new File(Objects.requireNonNull(file.getOriginalFilename()));
+        FileOutputStream fos = new FileOutputStream(convertedFile);
+        fos.write(file.getBytes());
+        fos.close();
+        return convertedFile;
+    }
 }
