b14e4f0e5538ed837e879613a8e63ae20f2ae06a
aathil12346 <129156477+aathil12346@users.noreply.github.com>
2024-06-13 17:42:51 +0100

    created a bankResponseDto class which will be the response of the bank to every request made

--- account-services/src/main/java/com/bankingapp/account_services/controller/BankAccountController.java
+++ account-services/src/main/java/com/bankingapp/account_services/controller/BankAccountController.java
@@ -1,9 +1,12 @@
 package com.bankingapp.account_services.controller;
 
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("v1/account-services")
 public class BankAccountController {
+
+
 }
---
+++ account-services/src/main/java/com/bankingapp/account_services/dto/BankResponseDto.java
@@ -0,0 +1,21 @@
+package com.bankingapp.account_services.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class BankResponseDto {
+
+    private String statusCode;
+    private String message;
+    private String accountNumber;
+    private String accountStatus;
+    private BigDecimal accountBalance;
+}
