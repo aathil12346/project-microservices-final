6645406df9f47d1b75f2e56f077d678af8928152
aathil12346 <129156477+aathil12346@users.noreply.github.com>
2024-06-13 17:10:35 +0100

    made some changes to the createBankAccountRequestDto class, also added a validation dependancy which helps with validating fields

--- account-services/src/main/java/com/bankingapp/account_services/dto/CreateBankAccountRequestDto.java
+++ account-services/src/main/java/com/bankingapp/account_services/dto/CreateBankAccountRequestDto.java
@@ -1,6 +1,10 @@
 package com.bankingapp.account_services.dto;
 
 import com.bankingapp.account_services.entity.AccountType;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
