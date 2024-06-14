74b7aa98caf58efcb15675a055e1dcb6f5c6f2c2
aathil12346 <129156477+aathil12346@users.noreply.github.com>
2024-06-14 22:31:09 +0100

    created the verification token class

---
+++ account-services/src/main/java/com/bankingapp/account_services/entity/VerificationToken.java
@@ -0,0 +1,29 @@
+package com.bankingapp.account_services.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+@Entity
+@Table(name = "tokens")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class VerificationToken {
+
+    @jakarta.persistence.Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long Id;
+    private String token;
+    @CreationTimestamp
+    private LocalDateTime createdDate;
+    private LocalDateTime expiryDate;
+    @OneToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+}
