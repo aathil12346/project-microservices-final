9d6c03b2c7aa2aaeb09a02b2b9e8b7c6fec59754
aathil12346 <129156477+aathil12346@users.noreply.github.com>
2024-06-25 00:23:39 +0100

    created the login information dto class

---
+++ account-services/src/main/java/com/bankingapp/account_services/dto/LoginInformationDto.java
@@ -0,0 +1,27 @@
+package com.bankingapp.account_services.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class LoginInformationDto {
+    @NotEmpty(message = "Email cannot be empty")
+    @NotBlank(message = "Email cannot be blank")
+    @Email(regexp = "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$")
+    private String email;
+    @Pattern(
+            regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\\S+$).{8,20}$",
+            message = "Password should have at least one: small letter, one capital letter, one digit, one special character, and be in the range of 8 to 20 characters."
+    )
+    @NotBlank(message = "Please set a password")
+    private String password;
+}
