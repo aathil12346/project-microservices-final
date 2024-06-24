81c5a127cc4d4e1e6c07b1ad3c669c6cfbfc72d1
aathil12346 <129156477+aathil12346@users.noreply.github.com>
2024-06-25 00:00:24 +0100

    added a password encoder method which will encode all the passwords before saving to the database

--- account-services/src/main/java/com/bankingapp/account_services/config/SecurityConfig.java
+++ account-services/src/main/java/com/bankingapp/account_services/config/SecurityConfig.java
@@ -2,19 +2,31 @@ package com.bankingapp.account_services.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
 
     @Bean
+    public static PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
 
         httpSecurity.csrf(AbstractHttpConfigurer::disable)
-                .authorizeHttpRequests((authorize)-> authorize.anyRequest().authenticated())
+                .authorizeHttpRequests((authorize)-> authorize.requestMatchers(HttpMethod.POST
+                ,"v1/account-services/create-account").permitAll()
+                        .requestMatchers(HttpMethod.GET,"v1/account-services/verify").permitAll()
+                        .anyRequest().authenticated())
+
                 .httpBasic(Customizer.withDefaults());
         return httpSecurity.build();
     }
