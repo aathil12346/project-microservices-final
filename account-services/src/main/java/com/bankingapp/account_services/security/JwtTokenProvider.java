d09c3f16d3a6ad690016c9f359488103f071e3b9
aathil12346 <129156477+aathil12346@users.noreply.github.com>
2024-06-29 23:49:57 +0100

    created the Jwt authentication filter class

--- account-services/src/main/java/com/bankingapp/account_services/security/JwtTokenProvider.java
+++ account-services/src/main/java/com/bankingapp/account_services/security/JwtTokenProvider.java
@@ -4,10 +4,12 @@ import com.bankingapp.account_services.exception.AppException;
 import io.jsonwebtoken.*;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
 
 import java.security.Key;
 import java.util.Date;
@@ -34,6 +36,15 @@ public class JwtTokenProvider {
 
     }
 
+    public String getUsername(String token){
+        Claims claims = Jwts.parserBuilder().setSigningKey(generateKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+
+        return claims.getSubject();
+    }
+
     public boolean validateToken(String token){
 
         try {
@@ -53,6 +64,16 @@ public class JwtTokenProvider {
         }
 
 
+    }
+
+    public String extractJwtFromRequest(HttpServletRequest request){
+        String bearerToken = request.getHeader("Authorization");
+
+        if (StringUtils.hasText(bearerToken) && bearerToken.startsWith("Bearer ")){
+            return bearerToken.substring(7,bearerToken.length());
+        }
+
+        return null;
     }
     private Key generateKey(){
          return Keys.hmacShaKeyFor(Decoders.BASE64.decode(secret));
