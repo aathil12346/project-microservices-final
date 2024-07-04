cabce760a082e3ed95d450a08afdd120896fac8f
aathil12346 <129156477+aathil12346@users.noreply.github.com>
2024-07-04 23:48:55 +0100

    created amount transfer request dto class

---
+++ transaction-services/src/main/java/com/bankingapp/transaction_services/dto/AmountTransferRequestDto.java
@@ -0,0 +1,16 @@
+package com.bankingapp.transaction_services.dto;
+
+import lombok.*;
+
+import java.math.BigDecimal;
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class AmountTransferRequestDto {
+
+    private String recipientAccountNumber;
+    private String senderAccountNumber;
+    private BigDecimal amountToBeTransferred;
+}
